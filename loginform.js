67b20a8f2762e791ed8dd1aaa95c384d122cd72d
youssefbaya12071207 <85020806+youssefbaya12071207@users.noreply.github.com>
2021-05-30 00:13:41 +0100

    Add files via upload

---
+++ loginform.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Router = express.Router();
+const User= require('../modle/User');
+Router.post('/routes/inscription',async (req,res)=>{
+   const usr = new User({
+      nom : req.body.nom,
+      prenom : req.body.prenom,
+      adress : req.body.adress,
+   });
+}
+
+)
+module.exports = Router;
